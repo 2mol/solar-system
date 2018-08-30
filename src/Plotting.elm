0cc496836acb24f075610519f94e5e2f5d448565
2mol <2mol@users.noreply.github.com>
2018-08-30 16:29:05 +0200

    better text anchoring, style improvements

--- src/Plotting.elm
+++ src/Plotting.elm
@@ -123,8 +123,8 @@ draw ( width, height ) color (Plot p) =
         avgString =
             Round.round 1 avg
 
-        avgLabelPos =
-            String.fromInt <| labelPosRight width avgString
+        rightAlignPos =
+            String.fromInt (width - 2)
 
         yMinString =
             String.fromFloat yMin
@@ -139,8 +139,12 @@ draw ( width, height ) color (Plot p) =
             case p.range of
                 Fixed _ ->
                     Svg.g []
-                        [ Svg.text_ [ S.x avgLabelPos, S.y hAvgLabel ] [ Svg.text avgString ]
-                        , Svg.line [ S.x1 "0", S.y1 hstrAvg, S.x2 wstr, S.y2 hstrAvg, S.stroke "#bbb" ] []
+                        [ Svg.text_
+                            [ S.x rightAlignPos, S.textAnchor "end", S.y hAvgLabel ]
+                            [ Svg.text avgString ]
+                        , Svg.line
+                            [ S.x1 "0", S.y1 hstrAvg, S.x2 wstr, S.y2 hstrAvg, S.stroke "black", S.opacity "0.35" ]
+                            []
                         ]
 
                 _ ->
@@ -154,23 +158,13 @@ draw ( width, height ) color (Plot p) =
         , S.fontFamily "Courier New"
         , S.fontSize "12"
         ]
-        [ Svg.text_ [ S.x "0.3em", S.y "1em" ] [ Svg.text p.name ]
-        , Svg.line [ S.x1 "0", S.y1 hstrHalf, S.x2 wstr, S.y2 hstrHalf, S.stroke "black" ] []
+        [ Svg.line [ S.x1 "0", S.y1 hstrHalf, S.x2 wstr, S.y2 hstrHalf, S.stroke "black", S.opacity "0.25" ] []
         , Svg.polyline [ S.fill "none", S.stroke color, S.points coordString ] []
         , avgLine
-        , Svg.text_ [ S.x (String.fromInt <| labelPosRight width yMaxString), S.y "1em" ] [ Svg.text yMaxString ]
-        , Svg.text_ [ S.x (String.fromInt <| labelPosRight width yMinString), S.y bottomPos ] [ Svg.text yMinString ]
+        , Svg.text_ [ S.x "0.3em", S.y "1em" ] [ Svg.text p.name ]
+        , Svg.text_ [ S.x rightAlignPos, S.textAnchor "end", S.y "1em" ] [ Svg.text yMaxString ]
+        , Svg.text_ [ S.x rightAlignPos, S.textAnchor "end", S.y bottomPos ] [ Svg.text yMinString ]
         ]
-
-
-labelPosRight : Int -> String -> Int
-labelPosRight width label =
-    width - (round <| toFloat (String.length label) * 7.22) - 3
-
-
-
--- labelPosBottom : Int -> Int
--- labelPosBottom height =
 
 
 median : Array Float -> Float
